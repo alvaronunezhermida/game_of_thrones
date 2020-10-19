b4c364c6985bcbe233f9987a2626758c4fbc7217
alvaronunezhermida <57688280+alvaronunezhermida@users.noreply.github.com>
2020-10-19 22:25:16 +0200

    Project structure (#1)
    * Create launcher activity
    
    * Added data models
    
    Co-authored-by: Álvaro Núñez Hermida <alvaro.nunezhermida@plexus.es>

---
+++ app/src/main/java/com/alvaronunez/gameofthrones/presentation/ui/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.alvaronunez.gameofthrones.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alvaronunez.gameofthrones.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
