2f8f698823188806badac44e92d1c5022ec04bba
Álvaro Núñez Hermida <alvaro.nunezhermida@plexus.es>
2020-10-22 00:22:16 +0200

    Fixed koin modules implementation

--- app/src/main/java/com/alvaronunez/gameofthrones/di/module.kt
+++ app/src/main/java/com/alvaronunez/gameofthrones/di/module.kt
@@ -8,6 +8,7 @@ import com.alvaronunez.gameofthrones.data.source.RemoteDataSource
 import com.alvaronunez.gameofthrones.domain.usecases.GetCategories
 import com.alvaronunez.gameofthrones.presentation.data.database.AppDatabase
 import com.alvaronunez.gameofthrones.presentation.data.database.RoomDataSource
+import com.alvaronunez.gameofthrones.presentation.data.service.Service
 import com.alvaronunez.gameofthrones.presentation.data.service.ServiceDataSource
 import com.alvaronunez.gameofthrones.presentation.ui.SplashActivity
 import com.alvaronunez.gameofthrones.presentation.ui.SplashPresenter
@@ -15,14 +16,15 @@ import org.koin.android.ext.koin.androidApplication
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 fun Application.initDI() {
     startKoin {
-        androidLogger()
+        androidLogger(Level.NONE)
         androidContext(this@initDI)
-        modules(listOf(appModule, dataModule, scopesModule))
+        modules(listOf(appModule, dataModule, domainModule, presentersModule))
     }
 }
 
@@ -30,16 +32,18 @@ private val appModule = module {
     single(named("baseApiUrl")) { androidApplication().getString(R.string.baseApiUrl) }
     single { AppDatabase.build(get()) }
     factory<LocalDataSource> { RoomDataSource(get()) }
-    factory<RemoteDataSource> { ServiceDataSource(get(named("baseApiUrl"))) }
+    factory { Service(get(named("baseApiUrl"))) }
+    factory<RemoteDataSource> { ServiceDataSource(get()) }
 }
 
 private val dataModule = module {
     factory { Repository(get(), get()) }
 }
 
-private val scopesModule = module {
-    scope(named<SplashActivity>()){
-        factory { SplashPresenter(get()) }
-        scoped { GetCategories(get()) }
-    }
+private val domainModule = module {
+    factory { GetCategories(get()) }
+}
+
+private val presentersModule = module {
+    factory { SplashPresenter(get()) }
 }
